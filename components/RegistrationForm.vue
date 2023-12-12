f9b5931c1f9a8ebec9c92453fd6ec75877616129
Kitcht <oteamton@gmail.com>
2023-12-12 15:01:33 +0700

    Done the require

--- components/RegistrationForm.vue
+++ components/RegistrationForm.vue
@@ -1,18 +1,44 @@
-6<template>
+6
+<template>
   <h2 class="text-4xl text-center mb-4 text-white">ลงทะเบียน</h2>
   <div class="breadcrumb flex gap-1">
-    <a href="#" class="bg-green-700 p-4 text-white" @click="currStep = 1"
-      :class="{ 'border-white border-2': currStep === 1 }">ส่วนที่ 1</a>
-    <a href="#" class="bg-green-700 p-4 text-white"
-      :class="{ 'disabled-link': !firstPageValid, 'border-white border-2': currStep === 2 }"
-      @click="firstPageValid ? currStep = 2 : $event.preventDefault()">ส่วนที่ 2</a>
+    <a
+      href="#"
+      class="p-4 text-white"
+      @click="currStep = 1"
+      :class="{
+        'bg-gray-500': !firstPageValid,
+        'bg-green-700': firstPageValid,
+        'border-white border-2': currStep === 1,
+      }"
+      >ส่วนที่ 1</a
+    >
+    <a
+      href="#"
+      class="p-4 text-white"
+      :class="{
+        'disabled-link': !firstPageValid,
+        'bg-gray-500': !secondPageValid,
+        'bg-green-700': secondPageValid,
+        'border-white border-2': currStep === 2,
+      }"
+      @click="firstPageValid ? (currStep = 2) : $event.preventDefault()"
+      >ส่วนที่ 2</a
+    >
   </div>
 
-  <form @submit.prevent="submitRegistration" class="flex flex-col justify-center w-96 gap-1 p-4 input">
+  <form
+    @submit.prevent="submitRegistration"
+    class="flex flex-col justify-center w-96 gap-1 p-4 input"
+  >
     <!-- page 1 -->
     <div v-if="currStep === 1" class="flex flex-col gap-2">
       <label for="gender">เพศ</label>
-      <div id="gender" class="flex gap-2 border-orange-300 border-2 p-2" :class="{ 'border-none': isGenderSelected }">
+      <div
+        id="gender"
+        class="flex gap-2 border-orange-300 border-2 p-2"
+        :class="{ 'border-none': isGenderSelected }"
+      >
         <label>
           <input type="radio" v-model="formData.gender" value="ชาย" />
           ชาย
@@ -29,24 +55,43 @@
 
       <label for="name">ชื่อจริง</label>
       <input v-model="formData.name" type="text" id="name" required />
-      <span class="text-orange-300" v-if="!isNameValid">Please enter at least 4 characters</span>
+      <span class="text-orange-300" v-if="!isNameValid"
+        >Please enter at least 4 characters</span
+      >
 
       <label for="surname">นามสกุล</label>
       <input v-model="formData.surname" type="text" id="surname" required />
-      <span class="text-orange-300" v-if="!isSurnameValid">Please enter at least 4 characters</span>
+      <span class="text-orange-300" v-if="!isSurnameValid"
+        >Please enter at least 4 characters</span
+      >
 
       <label for="date_of_birth">วัน เดือน ปีเกิด</label>
       <!-- Use a date picker for date_of_birth -->
-      <input v-model="formData.date_of_birth" type="date" id="date_of_birth" required />
-      <span class="text-orange-300" v-if="!isDateofBirthSelected">Please select a date</span>
+      <input
+        v-model="formData.date_of_birth"
+        type="date"
+        id="date_of_birth"
+        required
+      />
+      <span class="text-orange-300" v-if="!isDateofBirthSelected"
+        >Please select a date</span
+      >
       <label for="phone">เบอร์โทรศัพท์</label>
       <input v-model="formData.phone" type="text" id="phone" required />
-      <span class="text-orange-300" v-if="!isPhoneValid">Please enter valid phone</span>
+      <span class="text-orange-300" v-if="!isPhoneValid"
+        >Please enter valid phone</span
+      >
 
-      <button :disabled="!firstPageValid" :class="{
-        'bg-gray-500': !firstPageValid,
-        'bg-blue-500 hover:bg-blue-700 active:bg-blue-800': firstPageValid,
-      }" type="button" @click="NextPage" class="text-white font-bold py-2 px-4 rounded mt-4 max-w-fit self-center">
+      <button
+        :disabled="!firstPageValid"
+        :class="{
+          'bg-gray-500': !firstPageValid,
+          'bg-blue-500 hover:bg-blue-700 active:bg-blue-800': firstPageValid,
+        }"
+        type="button"
+        @click="NextPage"
+        class="text-white font-bold py-2 px-4 rounded mt-4 max-w-fit self-center"
+      >
         หน้าถัดไป
       </button>
     </div>
@@ -55,39 +100,80 @@
     <div v-if="currStep === 2" class="flex flex-col gap-2 mb-4">
       <label for="email">อีเมล์</label>
       <input v-model="formData.email" type="email" id="email" required />
-      <span class="text-orange-300" v-if="!isEmailValid">Please enter valid email</span>
+      <span class="text-orange-300" v-if="!isEmailValid"
+        >Please enter valid email</span
+      >
 
       <label for="address">ที่อยู่</label>
       <input v-model="formData.address" type="text" id="address" required />
-      <span class="text-orange-300" v-if="!isEmailValid">Please enter address</span>
+      <span class="text-orange-300" v-if="!isEmailValid"
+        >Please enter address</span
+      >
 
       <label for="province">จังหวัด</label>
       <input v-model="formData.province" type="text" id="province" required />
-      <span class="text-orange-300" v-if="!isEmailValid">Please enter province</span>
+      <span class="text-orange-300" v-if="!isEmailValid"
+        >Please enter province</span
+      >
 
       <label for="state">แขวง</label>
       <input v-model="formData.state" type="text" id="state" required />
-      <span class="text-orange-300" v-if="!isEmailValid">Please enter state</span>
+      <span class="text-orange-300" v-if="!isEmailValid"
+        >Please enter state</span
+      >
     </div>
+
     <div v-if="currStep === 2" class="flex justify-center gap-2">
-      <button :disabled="!secondPageValid" :class="{
-        'bg-gray-500': !secondPageValid,
-        'bg-blue-500 hover:bg-blue-700 active:bg-blue-800 active:scale-90': secondPageValid,
-      }" class="text-white font-bold py-2 px-4 rounded mt-2" type="submit">
-        {{ loading ? "กำลังดำเนินการ..." : "ยืนยัน" }}
+      <button
+        type="button"
+        :disabled="!secondPageValid"
+        :class="{
+          'bg-gray-500': !secondPageValid,
+          'bg-blue-500 hover:bg-blue-700 active:bg-blue-800 active:scale-90':
+            secondPageValid,
+        }"
+        class="text-white font-bold py-2 px-4 rounded mt-2"
+        @click="showModal = true"
+      >
+        ยืนยัน
       </button>
-      <button type="button" @click="cancelForm"
-        class="bg-red-500 hover:bg-red-700 active:bg-red-800 active:scale-90 text-white font-bold py-2 px-4 rounded mt-2">
-        ยกเลิก
+      <button
+        type="button"
+        @click="currStep = 1"
+        class="bg-gray-500 hover:bg-gray-700 active:bg-gray-800 active:scale-90 text-white font-bold py-2 px-4 rounded mt-2"
+      >
+        ย้อนกลับ
       </button>
     </div>
 
-    <span v-if="loading" class="spinner self-center"></span>
-    <span v-else></span>
-    <div v-if="resultMsg" class="overlay"></div>
-    <div v-if="resultMsg" class="result-box">
-      {{ resultMsg }}
-      <button @click="resultMsg = ''" class="close-btn">✕</button>
+    <!-- modal pop up -->
+    <div v-if="showModal" class="modal">
+      <span v-if="loading" class="spinner self-center"></span>
+      <div v-else class="modal-content">
+        <p>ข้อมูลถูกต้อง?</p>
+        <div class="flex gap-2">
+          <button
+            @click="submitRegistration"
+            class="bg-blue-500 hover:bg-blue-700 active:bg-blue-800 active:scale-90 text-white font-bold py-2 px-4 rounded mt-2"
+          >
+            {{ loading ? "กำลังดำเนินการ..." : "ยืนยัน" }}
+          </button>
+          <button
+            type="button"
+            @click="showModal = false"
+            class="bg-red-500 hover:bg-red-700 active:bg-red-800 active:scale-90 text-white font-bold py-2 px-4 rounded mt-2"
+          >
+            ยกเลิก
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div v-if="resultMsg" class="overlay">
+      <div v-if="resultMsg" class="result-box">
+        {{ resultMsg }}
+        <button @click="resultMsg = ''" class="close-btn">✕</button>
+      </div>
     </div>
   </form>
 </template>
@@ -96,6 +182,7 @@
 import { ref, computed } from "vue";
 const currStep = ref(1);
 const loading = ref(false);
+const showModal = ref(false);
 const resultMsg = ref("");
 const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
 const nameRegex = /^[A-Za-z\u0E00-\u0E7F]+$/;
@@ -106,10 +193,6 @@ const NextPage = () => {
   if (firstPageValid.value) {
     currStep.value++;
   }
-};
-
-const cancelForm = () => {
-  window.close();
 };
 
 const formData = ref({
@@ -159,14 +242,20 @@ const isAddressValid = computed(() => {
 
 const isStateValid = computed(() => {
   return addressRegex.test(formData.value.state);
-})
+});
 
 const isProvinceValid = computed(() => {
   return addressRegex.test(formData.value.province);
-})
+});
 
 const firstPageValid = computed(() => {
-  if (isPhoneValid.value && isNameValid.value && isSurnameValid.value && isDateofBirthSelected.value && isGenderSelected.value == true) {
+  if (
+    isPhoneValid.value &&
+    isNameValid.value &&
+    isSurnameValid.value &&
+    isDateofBirthSelected.value &&
+    isGenderSelected.value == true
+  ) {
     return true;
   } else {
     return false;
@@ -174,7 +263,12 @@ const firstPageValid = computed(() => {
 });
 
 const secondPageValid = computed(() => {
-  if (isProvinceValid.value && isStateValid.value && isAddressValid.value && isEmailValid.value == true) {
+  if (
+    isProvinceValid.value &&
+    isStateValid.value &&
+    isAddressValid.value &&
+    isEmailValid.value == true
+  ) {
     return true;
   } else {
     return false;
@@ -204,8 +298,9 @@ const submitRegistration = async () => {
       resultMsg.value = "Registration successful";
       console.log("Registration successful:", responseDate);
     } else {
-      resultMsg.value = `Registration failed: ${responseDate.error || response.error
-        }`;
+      resultMsg.value = `Registration failed: ${
+        responseDate.error || response.error
+      }`;
       console.error(
         "Registration failed:",
         responseData.error || response.statusText
@@ -216,6 +311,7 @@ const submitRegistration = async () => {
     console.error("Registration failed:", error.message);
   } finally {
     loading.value = false;
+    showModal.value = false;
   }
 };
 </script>
@@ -248,6 +344,26 @@ template {
 .disabled-link {
   pointer-events: none;
   cursor: default;
+}
+
+/* modal pop-up */
+.modal {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal-content {
+  background-color: white;
+  padding: 20px;
+  border-radius: 5px;
+  text-align: center;
 }
 
 .result-box {

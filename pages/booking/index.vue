e30aba4537f55fc5e1291f52fb879a5daa062d7a
Kitcht <oteamton@gmail.com>
2024-01-09 16:29:27 +0700

    Added: booking page

--- pages/booking/index.vue
+++ pages/booking/index.vue
@@ -1,13 +1,15 @@
 <template>
-  <title>เลือกผู้ให้การรักษา - SWU CLINIC</title>
-  <Banner />
-    
+  <div class="booking-page flex flex-col justify-center">
+    <title>เลือกผู้ให้การรักษา - SWU CLINIC</title>
+    <Banner />
+    <AppointmentList />
+  </div>
 </template>
 
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <style>
-
+.booking-page {
+  min-height: 100svh;
+}
 </style>

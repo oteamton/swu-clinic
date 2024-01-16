14ce46b0a452d0de8d9b8df2fcf77aa563c820ec
Kitcht <oteamton@gmail.com>
2024-01-16 16:55:43 +0700

    Big component to work on

--- components/UserPanel.vue
+++ components/UserPanel.vue
@@ -6,35 +6,46 @@
     @mousedown="onMouseDown"
     ref="panel"
   >
+    <div v-if="isLoading" class="loading-spinner">Loading. . .</div>
     <div v-show="!isCollapsed" class="user-content">
-      <div class="user-info bg-pink-500 rounded-md">
-        <img src="https://via.placeholder.com/150" alt="User Avatar" class="avatar" />
+      <div class="user-info flex flex-col gap-0.5 bg-pink-500 rounded-md">
+        <img
+          src="https://via.placeholder.com/150"
+          alt="User Avatar"
+          class="avatar"
+        />
         <h2>{{ userData?.name }} {{ userData?.surname }}</h2>
-        <p>{{ userData?.email }}</p>
+        <!-- <p>{{ userData?.email }}</p> -->
         <p><span>Born on </span>{{ formattedDate }}</p>
         <p><span>Phone: </span>{{ userData?.phone }}</p>
       </div>
-
       <div class="user-actions">
         <button @click.stop="logout">Logout</button>
-        <!-- Further user actions or features can be added here -->
       </div>
     </div>
-    <div class="toggle-area" @click="toggleCollapse"></div>
   </div>
 </template>
 
 <script setup lang="ts">
+const isLoading = ref(false);
 const userData = ref();
 const message = ref("");
-const props = defineProps({
-  token: String,
-});
 const isCollapsed = ref(false);
 const panel = ref<HTMLElement | null>(null);
 const panelPosition = ref({ x: 0, y: 0 });
 let clickStartTime = 0;
 const checkThreshold = 200;
+
+// Draggable functionality
+let startX = 0,
+  startY = 0,
+  deltaX = 0,
+  deltaY = 0;
+let isDragging = false;
+
+const props = defineProps({
+  token: String,
+});
 const formattedDate = computed(() => {
   return userData.value?.date_of_birth
     ? new Date(userData.value.date_of_birth).toLocaleDateString()
@@ -50,12 +61,7 @@ const logout = (event: Event) => {
   alert("Logged out");
 };
 
-// Draggable functionality
-let startX = 0,
-  startY = 0,
-  deltaX = 0,
-  deltaY = 0;
-let isDragging = false;
+
 
 const onMouseDown = (event: MouseEvent) => {
   isDragging = true;
@@ -100,14 +106,15 @@ onUnmounted(() => {
 
 // Get user data
 const getUserData = async () => {
+  isLoading.value = true;
   try {
     const response = await fetch(
-      "http://localhost:8080/api/v1/user-medical-record",
+      "http://localhost:8080/api/v1/users/get-user-data",
       {
         method: "GET",
-        headers: {
-          Authorization: `Bearer ${props.token}`,
-        },
+        // headers: {
+        //   Authorization: `Bearer ${props.token}`,
+        // },
       }
     );
 
@@ -120,13 +127,12 @@ const getUserData = async () => {
   } catch (error) {
     // console.error('Error fetching medical data:', error);
     message.value = "An error occurred while fetching medical data.";
+  } finally {
+    isLoading.value = false;
   }
 };
 
 onMounted(() => {
-  if (panel.value) {
-    panel.value.addEventListener("mousedown", onMouseDown);
-  }
   getUserData();
 });
 </script>
@@ -144,21 +150,11 @@ onMounted(() => {
   will-change: transform;
 }
 
-.toggle-area {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  cursor: pointer;
-  z-index: 10;
-}
-
 .user-panel.collapsed {
   width: 50px;
   height: 50px;
   border-radius: 50%;
-  background-color: #fff;
+  background-color: pink;
   overflow: hidden;
   padding: 0;
 }

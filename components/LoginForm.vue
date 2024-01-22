75f995db925708c6b02990a785b457cc0e0d0111
Kitcht <oteamton@gmail.com>
2024-01-22 13:37:03 +0700

    Adjested: main function, some UI

--- components/LoginForm.vue
+++ components/LoginForm.vue
@@ -1,50 +1,69 @@
 <template>
   <h2 class="text-4xl text-center mb-4 text-white">เข้าสู่การจอง</h2>
-  <form @submit.prevent="submitLogin" class="">
+  <form @submit.prevent="submitLogin">
     <div class="flex flex-col justify-center w-96 gap-2 p-4 text-white">
-      <label for="phone">Phone numbers</label>
-      <input type="text" id="phone" v-model="login.phone" />
+      <label for="phone">หมายเลขโทรศัพท์ (Phone number)</label>
+      <input type="text" id="phone" v-model.trim="login.phone" />
 
-      <label for="otp">OTP</label>
-      <input type="password" id="otp" v-model="login.otp" />
+      <label for="cardId">รหัสบัตรประชาชน (Card ID)</label>
+      <input type="text" id="cardId" v-model.trim="login.cardId" />
     </div>
 
     <div class="text-white login-btn-container">
       <button
-        type="submit"
+        @click="submitLogin"
         class="py-2 px-4 rounded bg-blue-500 active:bg-blue-800 active:scale-90"
       >
         Login
-      </button>
-      <button
-        type="button"
-        @click="resendOTP"
-        class="text-gray-300 p-0 active:scale-90"
-      >
-        Resend OTP
       </button>
     </div>
   </form>
 </template>
 
-<script setup>
-import { ref } from "vue";
+<script setup lang="ts">
+const loading = ref(false);
+const resultMessage = ref("");
+
+interface ResponseData {
+  error?: string;
+  token?: string;
+}
 
 const login = ref({
   phone: "",
-  otp: "",
+  cardId: "",
 });
 
-const resendOTP = () => {
-  console.log("resend otp");
-};
+const submitLogin = async () => {
+  loading.value = true;
+  resultMessage.value = "";
+  try {
+    const response = await fetch("http://localhost:8080/api/v1/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(login.value),
+    });
 
-const submitLogin = () => {
-  console.log("submit login");
+    const responseData: ResponseData = await response.json();
+
+    if (response.ok) {
+      if (responseData.token) {
+        navigateTo(`/booking/${responseData.token}`);
+      } else {
+        resultMessage.value = "Registration failed no otp found";
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  } finally {
+    loading.value = false;
+  }
 };
 </script>
 
-<style>
+<style scoped>
 input {
   text-indent: 5px;
   border-radius: 6px;
@@ -70,4 +89,3 @@ input {
   justify-self: end;
 }
 </style>
- 
